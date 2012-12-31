af8304a786088f42e8f76257e54698c0e4e7a9fd
Periklis Ntanasis <pntanasis@gmail.com>
2012-12-31 02:59:43 +0200

    new layout and settings tested with asciiblog tmp-build

--- .asciiblog/layouts/archiveLine.sh
+++ .asciiblog/layouts/archiveLine.sh
@@ -5,4 +5,4 @@
 # $3 DAY
 # $4 file name
 # $5 article name
-echo "<p>$1-$2-$3: <a href="archive/$1/$2/$3/$4.html">$5</a></p>"
+echo "<li><a href="archive/$1/$2/$3/$4.html">$5, posted on $3-$2-$1</a></li>"
--- .asciiblog/layouts/archiveYearLine.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# $1 YEAR
-echo "<h3>$1</h3>"
